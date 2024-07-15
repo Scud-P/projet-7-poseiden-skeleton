d3b72ecd54dd55d35ed51cb2841a0ac4abd1195c
PScud <escudie.pierre@gmail.com>
2024-07-15 15:20:10 -0400

    Reorganised project

--- poseiden-skeleton/src/main/java/com/nnk/springboot/exceptions/UserNameAlreadyUsedException.java
+++
@@ -1,8 +0,0 @@
-package com.nnk.springboot.exceptions;
-
-public class UserNameAlreadyUsedException extends RuntimeException {
-
-    public UserNameAlreadyUsedException(String message) {
-        super(message);
-    }
-}
